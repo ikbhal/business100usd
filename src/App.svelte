84968353813f06ace5f865ea0544e10e64766f6c
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-05 17:19:24 +0530

    video

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,10 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<h1>Start Business with less than 100$</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vxs2Bj8zKXk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
